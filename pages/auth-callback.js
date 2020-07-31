f766342ae93dc8c4747327cd560a75595f28749c
Matt Jones <mattjones701@gmail.com>
2020-07-31 23:08:32 +1000

    WIP

--- pages/auth-callback.js
+++ pages/auth-callback.js
@@ -1,12 +1,9 @@
 import got from 'got';
-import queryString from 'query-string';
 import { useEffect } from 'react';
 
 export async function getServerSideProps({ query }) {
   const clientId = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
   const clientSecret = process.env.GITHUB_CLIENT_SECRET;
-
-  const { code } = queryString.parse(query);
 
   const { body } = await got.post(
     'https://github.com/login/oauth/access_token',
@@ -14,7 +11,7 @@ export async function getServerSideProps({ query }) {
       json: {
         client_id: clientId,
         client_secret: clientSecret,
-        code,
+        code: query.code,
       },
       responseType: 'json',
       headers: {
