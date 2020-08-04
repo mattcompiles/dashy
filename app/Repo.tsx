54ea101474cc49a99aab40d613ff2d5067c2905f
Matt Jones <mattjones701@gmail.com>
2020-08-04 15:01:58 +1000

    Update repo query

--- app/Repo.tsx
+++ app/Repo.tsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
-const REPO_QUERY = gql`
-  {
-    repository(name: "braid-design-system", owner: "seek-oss") {
+const GET_REPO = gql`
+  query GetRepo($name: String!, $owner: String!) {
+    repository(name: $name, owner: $owner) {
+      name
       pullRequests(states: OPEN, first: 10) {
         edges {
           node {
+            title
             mergeable
-            potentialMergeCommit {
-              status {
-                contexts {
-                  id
-                  state
-                  description
+            viewerDidAuthor
+            commits(first: 1) {
+              nodes {
+                commit {
+                  status {
+                    contexts {
+                      state
+                      description
+                      targetUrl
+                    }
+                    state
+                  }
                 }
-                state
               }
             }
-            title
           }
         }
       }
-      name
     }
   }
 `;
 
-export function Repo() {
-  const { loading, error, data } = useQuery(REPO_QUERY);
+interface RepoProps {
+  owner: string;
+  name: string;
+}
+export function Repo({ owner, name }: RepoProps) {
+  const { loading, error, data } = useQuery(GET_REPO, {
+    variables: { owner, name },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
